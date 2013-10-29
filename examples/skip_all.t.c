fe705e6f145ee29ba111680b8b7b37f4c309efd5
Julian Maurice <jajm@jajm.org>
2013-10-29 07:40:07 +0100

    Add two examples source files
    and rename the existing one so the corresponding executable has '.t'
    suffix

---
+++ examples/skip_all.t.c
@@ -0,0 +1,9 @@
+#include "tap.h"
+
+int main()
+{
+	skip_all("No tests in there.");
+
+	return 0;
+}
+
---
+++ examples/unknown_number_of_tests.t.c
@@ -0,0 +1,12 @@
+#include "tap.h"
+
+int main()
+{
+	ok(1, "1");
+	ok(0, "0");
+
+	done_testing();
+
+	return 0;
+}
+
