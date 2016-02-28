96ab4ed8d3c4ed189ec3ad44688f681374e69a46
Julian Maurice <jajm@jajm.org>
2016-02-28 16:22:10 +0100

    Add version and description to pkg-config file

--- libtap13.pc.cmake
+++ libtap13.pc.cmake
@@ -3,7 +3,7 @@ includedir=${prefix}/include/@PROJECT_NAME@
 libdir=${prefix}/lib
 
 Name: @PROJECT_NAME@
-Description: @PROJECT_DESCRIPTION@
+Description: C library to produce TAP13 output
 Version: @PROJECT_VERSION@
 Requires.private: yaml-0.1
 Libs: -L${libdir} -ltap13
